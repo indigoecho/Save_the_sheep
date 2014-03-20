9c48fefa311b46cdbd30defe301a60ea4890d8a4
indigoecho <laura.j.brook@orangehome.co.uk>
2014-03-20 21:18:40 +0000

    Background and Loaded Correctly

--- script.js
+++ script.js
@@ -2,16 +2,32 @@
 function draw() {
 	var canvas = document.getElementById('sheep');
 	var ctx = canvas.getContext('2d');
-	var imageObj = new Image();
-	imageObj.src = "sheep1.png";
+	var sheep1 = new Image();
+	sheep1.src = "sheep1.png";
+	var background = new Image();
+	background.src = "grass.png";
 	
-	imageObj.onload = function(){
-		var xs=[0,100,200,300];
-		var ys=[0,100,200,300]
-		xs.forEach(function (x) {
-			ys.forEach(function(y){
-				ctx.drawImage(imageObj, x, y);
-			});
-		});
+	whenAllLoaded([background, sheep1], function() {
+		tile(ctx, background, 400, 0, 0, 64, 64);	
+		tile(ctx, sheep1, 400, 0, 0, 100, 100);
+	});
+}
+
+function tile(ctx, image, canvasSize, startx, starty, deltax, deltay){
+	for(var x=0; x<canvasSize; x+=deltax){
+		for(var y=0; y<canvasSize; y+=deltay){
+			ctx.drawImage(image, x, y);
+		};
+	};
+}
+
+function whenAllLoaded(images, callback){
+	function isLoaded(image){
+		return image.complete;
+	};
+	if (images.every(isLoaded)) {
+		callback();
+	} else {
+		setTimeout(whenAllLoaded, 1000);
 	};
 }
